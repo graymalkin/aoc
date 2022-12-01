b417efd55cfbebd24628a6e262246aee80d7bb68
Simon Cooksey <simon@graymalk.in>
2022-12-01 14:22:17 +0000

    simplify day 1

--- 2022/01/puzzle1.rs
+++ 2022/01/puzzle1.rs
@@ -1,25 +1,25 @@
 use std::io;
 
 struct Elf {
-    food_items: Vec<u32>
+    food_items: u32
 }
 
 fn main() {
-    let mut elves = vec![Elf { food_items: vec![] }];
+    let mut elves = vec![Elf { food_items: 0 }];
     let lines = io::stdin().lines();
 
     for line in lines {
         match line.unwrap().parse::<u32>() {
-            Ok (kcal) => { elves.last_mut().unwrap().food_items.push(kcal) }
-            Err (_) => { elves.push(Elf { food_items: vec![] }) }
+            Ok (kcal) => { elves.last_mut().unwrap().food_items += kcal }
+            Err (_) => { elves.push(Elf { food_items: 0 }) }
         }
     }
 
     let highest_elf = elves.iter().fold(&elves[0], |max_elf, curr_elf| {
-        let max_elf_kcal : u32 = max_elf.food_items.iter().sum::<u32>();
-        let curr_elf_kcal : u32 = curr_elf.food_items.iter().sum::<u32>();
+        let max_elf_kcal : u32 = max_elf.food_items;
+        let curr_elf_kcal : u32 = curr_elf.food_items;
         if curr_elf_kcal > max_elf_kcal { &curr_elf }
         else { &max_elf }
     });
-    println!("{}", highest_elf.food_items.iter().sum::<u32>())
+    println!("{}", highest_elf.food_items)
 }
--- 2022/01/puzzle2.rs
+++ 2022/01/puzzle2.rs
@@ -1,29 +1,29 @@
 use std::io;
 
 struct Elf {
-    food_items: Vec<u32>
+    food_items: u32
 }
 
 fn main() {
-    let mut elves = vec![Elf { food_items: vec![] }];
+    let mut elves = vec![Elf { food_items: 0 }];
     let lines = io::stdin().lines();
 
     for line in lines {
         match line.unwrap().parse::<u32>() {
-            Ok (kcal) => { elves.last_mut().unwrap().food_items.push(kcal) }
-            Err (_) => { elves.push(Elf { food_items: vec![] }) }
+            Ok (kcal) => { elves.last_mut().unwrap().food_items += kcal }
+            Err (_) => { elves.push(Elf { food_items: 0 }) }
         }
     }
 
     elves.sort_by(|a, b| {
-        let a_kcal : u32 = a.food_items.iter().sum::<u32>();
-        let b_kcal : u32 = b.food_items.iter().sum::<u32>();
+        let a_kcal : u32 = a.food_items;
+        let b_kcal : u32 = b.food_items;
         b_kcal.partial_cmp(&a_kcal).unwrap()
     });
 
     let mut total_kcal = 0;
     for elf in &elves[0..3] {
-        total_kcal += elf.food_items.iter().sum::<u32>();
+        total_kcal += elf.food_items;
     }
 
     println!("{}", total_kcal)
