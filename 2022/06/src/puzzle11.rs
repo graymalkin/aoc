089b5d2e438d4b8a364f94cbeb05588062415547
Simon Cooksey <simon@graymalk.in>
2022-12-06 13:51:57 +0000

    day6

---
+++ 2022/06/src/puzzle11.rs
@@ -0,0 +1,29 @@
+use std::io;
+use std::collections::VecDeque;
+use std::collections::HashSet;
+
+const START_OF_MESSAGE_UNIQ : usize = 4;
+
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+
+    let mut last_four = VecDeque::new();
+    let mut position = 0;
+    for c in line.chars() {
+        last_four.push_front(c);
+        if last_four.len() > START_OF_MESSAGE_UNIQ {
+            last_four.pop_back();
+        } 
+
+        if last_four.len() == START_OF_MESSAGE_UNIQ {
+            if last_four.clone().into_iter().collect::<HashSet<_>>().into_iter().count() == START_OF_MESSAGE_UNIQ {
+                println!("{}", position + 1);
+                return;
+            }
+        }
+        position += 1;
+    }
+
+    println!("");
+}
