0ac7a8deb6585ed0bb6eabbc59ba2249181ff416
Simon Cooksey <scooksey@nvidia.com>
2025-12-03 10:07:15 +0000

    2025 day03

---
+++ 2025/day03/part1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import fileinput
+import functools
+import operator
+from itertools import groupby
+
+
+def highest_joltage(bank):
+    pos_highest = 0
+    val_highest = 0
+    for i in range(0, len(bank) - 1):
+        if bank[i] > val_highest:
+            pos_highest = i
+            val_highest = bank[i]
+    
+    tens = val_highest
+    bank = bank[pos_highest+1:]
+
+    pos_highest = 0
+    val_highest = 0
+    for i in range(0, len(bank)):
+        if bank[i] > val_highest:
+            pos_highest = i
+            val_highest = bank[i]
+    units = val_highest
+
+    return 10*tens + units
+
+def main():
+    input_data = []
+    highest_per_bank = []
+    for line in fileinput.input():
+        bank = [int(i) for i in line.strip()]
+        input_data.append(bank)
+        highest = highest_joltage(bank)
+        highest_per_bank.append(highest)
+        print("{}: {}".format(bank, highest))
+    
+    print(functools.reduce(operator.add, highest_per_bank, 0))
+
+main()
---
+++ 2025/day03/part2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import fileinput
+
+
+def highest_joltage(num_batteries, bank):
+    joltage = 0
+    for bat in range(num_batteries, 0, -1):
+        pos_highest = 0
+        val_highest = 0
+        for i in range(0, len(bank) - bat + 1):
+            if bank[i] > val_highest:
+                pos_highest = i
+                val_highest = bank[i]
+        
+        joltage = joltage*10 + val_highest
+        bank = bank[pos_highest+1:]
+
+    return joltage
+
+def main():
+    total_joltage = 0
+    for line in fileinput.input():
+        bank = [int(i) for i in line.strip()]
+        total_joltage += highest_joltage(12, bank)
+    
+    print(total_joltage)
+
+main()
