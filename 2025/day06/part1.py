0a7b6aba28b3681d7ebbed16b1d90c21a491ded2
Simon Cooksey <scooksey@nvidia.com>
2025-12-06 11:15:03 +0000

    2025 day06

---
+++ 2025/day06/part1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import fileinput
+import functools
+import operator
+
+
+
+def main():
+    worksheet = []
+    for line in fileinput.input():
+        worksheet += [line.split()]
+
+    grand_total = 0
+    for c in range(len(worksheet[0])):
+        math_problem = [int(worksheet[r][c]) for r in range(len(worksheet) - 1)]
+        if worksheet[-1][c] == '+':
+            grand_total += sum(math_problem)
+        elif worksheet[-1][c] == '*':
+            grand_total += functools.reduce(operator.mul, math_problem, 1)
+
+    print(grand_total)
+
+
+main()
---
+++ 2025/day06/part2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import fileinput
+import functools
+import re
+import operator
+
+
+def product(xs):
+    return functools.reduce(operator.mul, xs, 1)
+
+def main():
+    worksheet = []
+    for line in fileinput.input():
+        worksheet += [list(line)]
+
+    # Transpose the worksheet so the last column is the first row.
+    worksheet = list(zip(*worksheet))[::-1]
+
+    grand_total = 0
+    operands = []
+
+    for line in worksheet:
+        number_str = "".join(line[0:-1])
+        if number_str.strip() == "":
+            operands = []
+            continue
+        
+        operands.append(int(number_str))
+
+        if line[-1] == "*":
+            grand_total += product(operands)
+        if line[-1] == "+":
+            grand_total += sum(operands)
+    
+    print(grand_total)
+
+
+main()
